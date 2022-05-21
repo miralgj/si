827eacc31c965f9e145baafe5263f953bfd190a4
Gilberto Miralla-Flores <miralgj@users.noreply.github.com>
2022-05-21 18:00:24 -0400

    Ping router

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -1,19 +1,34 @@
 package router
 
 import (
+    "log"
     "net/http"
+    "encoding/json"
+
     "github.com/go-chi/chi/v5"
     "github.com/go-chi/chi/v5/middleware"
 )
 
 func New() *chi.Mux {
     r := chi.NewRouter()
+
+    // Set up middlewares
+    r.Use(middleware.RealIP)
     r.Use(middleware.Logger)
-    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("welcome"))
-    })
+    r.Use(middleware.Recoverer)
+
+    r.Get("/ping", ping)
     return r
 }
 
-func init() {
+func ping(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json")
+    resp := make(map[string]string)
+    resp["response"] = "pong!"
+    jsonResp, err := json.Marshal(resp)
+    if err != nil {
+        log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+    }
+    w.Write(jsonResp)
+    return
 }
