9c3d7379c924205c43979dd880906264b9391a0c
Gilberto Miralla-Flores <miralgj@users.noreply.github.com>
2022-05-30 15:46:22 -0400

    Convenience variables

--- cmd/si/main.go
+++ cmd/si/main.go
@@ -13,9 +13,15 @@ import (
     "github.com/urfave/cli/v2"
 )
 
-// Compile with --ldflags="-w -X main.Version=$(VERSION)"
-// to set version number
-var Version string
+var (
+    conf = config.Config
+
+    die = log.Fatal
+
+    // Compile with --ldflags="-w -X main.Version=$(VERSION)"
+    // to set version number
+    Version string
+)
 
 func main() {
     app := cli.NewApp()
@@ -27,22 +33,20 @@ func main() {
     app.Flags = config.GetFlags()
     err := app.Run(os.Args)
     if err != nil {
-        log.Fatal(err)
+        die(err)
     }
 }
 
 func cliActionHandler(c *cli.Context) error {
     r := router.NewRouter()
-    if (config.Config.Tls) {
-        err := http.ListenAndServeTLS(config.Config.Listen+":"+config.Config.Port, config.Config.TlsCert, config.Config.TlsKey, r)
-        if err != nil {
-            return err
-        }
+    var err error
+    if (conf.Tls) {
+        err = http.ListenAndServeTLS(conf.Listen+":"+conf.Port, conf.TlsCert, conf.TlsKey, r)
     } else {
-        err := http.ListenAndServe(config.Config.Listen+":"+config.Config.Port, r)
-        if err != nil {
-            return err
-        }
+        err = http.ListenAndServe(conf.Listen+":"+conf.Port, r)
+    }
+    if err != nil {
+        return err
     }
     return nil
 }
@@ -56,12 +60,12 @@ func cliBeforeHandler(c *cli.Context) error {
     } else if(c.IsSet("tls-cert") && c.IsSet("tls-key")) {
         // Verify both provided files exist
         if _, err := os.Stat(c.String("tls-cert")); errors.Is(err, os.ErrNotExist) {
-            log.Fatal(err.Error())
+            die(err.Error())
         }
         if _, err := os.Stat(c.String("tls-key")); errors.Is(err, os.ErrNotExist) {
-            log.Fatal(err.Error())
+            die(err.Error())
         }
-        config.Config.Tls = true
+        conf.Tls = true
     }
     // Get commands from the cli context
     if len(c.StringSlice("command")) > 0 {
@@ -69,10 +73,10 @@ func cliBeforeHandler(c *cli.Context) error {
         for _, path := range c.StringSlice("command") {
             // Verify file exists before adding it
             if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
-                log.Fatal(err.Error())
+                die(err.Error())
             }
             name := filepath.Base(path)
-            config.Config.Commands[name] = path
+            conf.Commands[name] = path
         }
     }
     return nil
